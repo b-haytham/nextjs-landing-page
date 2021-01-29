7a561084448ad89ad6b0ee693ba57b4aa8df6cbc
b-haytham <boussarsarhaytham@gmail.com>
2021-01-29 22:20:06 +0100

    setup tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,9 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	return <h1>Home</h1>;
+	return (
+		<div className="container bg-black">
+			<h1 className="text-white font-bold">hello</h1>
+		</div>
+	);
 }
