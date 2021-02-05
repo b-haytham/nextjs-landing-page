cf1aa8734c2d761bdf3b21043b24ac5f6961a568
b-haytham <boussarsarhaytham@gmail.com>
2021-02-05 14:39:33 +0100

    fix footer responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,9 @@
 //#FFD700
 //#FFD700
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import useGreaterThan from "../utils/useGreaterThan";
 import AnimatedListItem from "./Animated/AnimatedListItem";
 import FooterBottomComponent from "./FooterBottomComponent";
 
@@ -11,21 +12,24 @@ interface FooterProps {}
 const Footer: React.FC<FooterProps> = () => {
   const { theme } = useContext(ThemeContext);
 
+  const isGreaterThan = useGreaterThan(764)
+
   return (
     <Box
       bgColor="#000000"
-      padding={50}
+      padding={isGreaterThan ? 50 : '50px 10px'}
       borderRadius={theme === "DARK" ? 0 : "10px 10px 0 0"}
       boxShadow="5px 9px 50px -23px #FFD700"
     >
-      <Flex
-        justifyContent="space-between"
+      {/* <Flex
+        justifyContent={isGreaterThan ? "space-between": 'space-evenly'}
         alignItems="center"
         marginBottom={50}
         wrap="wrap"
-      >
+      > */}
+      <SimpleGrid columns={isGreaterThan ? 4 : 2}   marginBottom={50}>
         <AnimatedListItem indx={0}>
-          <Flex flexDirection="column" margin="10px 15px">
+          <Flex flexDirection="column" margin="10px 15px" alignItems={!isGreaterThan && 'center'}>
             <Text
               color="#FFD700"
               fontFamily="Roboto"
@@ -50,7 +54,7 @@ const Footer: React.FC<FooterProps> = () => {
           </Flex>
         </AnimatedListItem>
         <AnimatedListItem indx={0.2}>
-          <Flex flexDirection="column" margin="10px 15px">
+          <Flex flexDirection="column" margin="10px 15px" alignItems={!isGreaterThan && 'center'}>
             <Text
               color="#FFD700"
               fontFamily="Roboto"
@@ -75,7 +79,7 @@ const Footer: React.FC<FooterProps> = () => {
           </Flex>
         </AnimatedListItem>
         <AnimatedListItem indx={0.4}>
-          <Flex flexDirection="column" margin="10px 15px">
+          <Flex flexDirection="column" margin="10px 15px" alignItems={!isGreaterThan && 'center'}>
             <Text
               color="#FFD700"
               fontFamily="Roboto"
@@ -100,7 +104,7 @@ const Footer: React.FC<FooterProps> = () => {
           </Flex>
         </AnimatedListItem>
         <AnimatedListItem indx={0.6}>
-          <Flex flexDirection="column" margin="10px 15px">
+          <Flex flexDirection="column" margin="10px 15px" alignItems={!isGreaterThan && 'center'}>
             <Text
               color="#FFD700"
               fontFamily="Roboto"
@@ -125,7 +129,7 @@ const Footer: React.FC<FooterProps> = () => {
           </Flex>
         </AnimatedListItem>
         <AnimatedListItem indx={0.8}>
-          <Flex flexDirection="column" margin="10px 15px">
+          <Flex flexDirection="column" margin="10px 15px" alignItems={!isGreaterThan && 'center'}>
             <Text
               color="#FFD700"
               fontFamily="Roboto"
@@ -151,7 +155,7 @@ const Footer: React.FC<FooterProps> = () => {
           </Flex>
         </AnimatedListItem>
         <AnimatedListItem indx={1}>
-          <Flex flexDirection="column" margin="10px 15px">
+          <Flex flexDirection="column" margin="10px 15px" alignItems={!isGreaterThan && 'center'}>
             <Text
               color="#FFD700"
               fontFamily="Roboto"
@@ -175,7 +179,8 @@ const Footer: React.FC<FooterProps> = () => {
             </Text>
           </Flex>
         </AnimatedListItem>
-      </Flex>
+        </SimpleGrid>
+      {/* </Flex> */}
       <AnimatedListItem indx={0.5}>
         <FooterBottomComponent />
       </AnimatedListItem>
